f2c353c6cfbca750c0f778a9882dc12cc000d6c9
Cloud User <centos@ip-172-31-84-159.ec2.internal>
2023-02-09 16:00:07 +0000

    Added SAN field to keystore

--- scripts/create-tls-certs.sh
+++ scripts/create-tls-certs.sh
@@ -3,52 +3,56 @@
 # SCRIPT: create-tls-certs.sh
 #
 
-certs_dir=/alluxio/certs
-store_password="changeme123"
+  certs_dir=/etc/alluxio/certs
+  store_password="changeme123"
 
-echo "Creating SSL keys for masters"
+  echo "Creating SSL keys for masters"
+  if [ -d $certs_dir ];
+  then
+    \rm -rf $certs_dir/*
+  fi
 
-if [ -d $certs_dir ];
-then
-  \rm -rf $certs_dir
-fi
-mkdir -p $certs_dir
-
-for fqdn in $(cat /opt/alluxio/conf/masters)
-do
-  echo "Creating keystore and truststore for all masters "
+  # Create the "Subject Alternative Name" (SAN) field
+  san=""
+  for fqdn in $(cat /opt/alluxio/conf/masters /opt/alluxio/conf/workers)
+  do
+    if [[ ${san} == "" ]];
+    then
+      san='dns:'${fqdn}
+    else
+      san=${san}',dns:'${fqdn}
+    fi
+  done
+  echo "Using Subect Alternative Names (SAN): " ${san}
 
   # Generate the keystore and certificate
   keytool -genkeypair -keypass ${store_password} -storepass ${store_password}  \
        -keyalg RSA -keysize 2048 \
-       -alias ${fqdn} \
-       -dname "CN=${fqdn}, OU=Alluxio, L=San Mateo, ST=CA, C=US" \
-       -keystore ${certs_dir}/all-masters-keystore.jks
-done
+       -alias all-alluxio-nodes \
+       -dname "CN=all-alluxio-nodes, OU=Alluxio, L=San Mateo, ST=CA, C=US" -ext san=${san} \
+       -keystore ${certs_dir}/all-alluxio-nodes-keystore.jks
 
-for fqdn in $(cat /opt/alluxio/conf/masters)
-do
-  # Export the certificate's public key to a certificate file
+  # Export the certificate’s public key to a certificate file
   keytool -export -rfc -storepass ${store_password} \
-       -alias ${fqdn} \
-       -keystore ${certs_dir}/all-masters-keystore.jks \
-       -file ${certs_dir}/${fqdn}.cert
+       -alias all-alluxio-nodes \
+       -keystore ${certs_dir}/all-alluxio-nodes-keystore.jks \
+       -file ${certs_dir}/all-alluxio-nodes.cert
 
-  # Import the certificate's public key to a truststore file
+  # Import the certificate’s public key to a truststore file
   keytool -import -noprompt -storepass ${store_password} \
-       -alias ${fqdn} \
-       -file  ${certs_dir}/${fqdn}.cert \
-       -keystore ${certs_dir}/${fqdn}-truststore.jks
-done
+       -alias all-alluxio-nodes \
+       -file  ${certs_dir}/all-alluxio-nodes.cert \
+       -keystore ${certs_dir}/all-alluxio-nodes-truststore.jks
 
 chmod 440 ${certs_dir}/*.jks ${certs_dir}/*.cert
-chmod 444 ${certs_dir}/alluxio-client-truststore.jks
+chmod 444 ${certs_dir}/all-alluxio-nodes-truststore.jks
 
 # List the contents of the trustore file
 echo; echo
-echo "Contents of trust store file: ${certs_dir}/alluxio-client-truststore.jks"
+echo "Contents of trust store file: ${certs_dir}/all-alluxio-nodes-truststore.jks"
 echo "(this file will be used by Alluxio client applications to connect to Alluxio daemons)"
 echo
-keytool -list -v -keystore ${certs_dir}/all-masters-keystore.jks -storepass $store_password | grep 'Owner:'
+keytool -list -v -keystore ${certs_dir}/all-alluxio-nodes-keystore.jks -storepass $store_password 
 
-# end of script
+# end of script 
+
