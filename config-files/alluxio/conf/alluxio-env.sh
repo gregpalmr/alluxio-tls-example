18ea42e86e5a5148f3a74068ff03a1909d61553c
Greg Palmer <gregpalmr@gmail.com>
2024-02-05 18:06:48 -0500

    Configured Trino for TSL

---
+++ config-files/alluxio/conf/alluxio-env.sh
@@ -0,0 +1,4 @@
+# Turn on Alluxio Debug mode (un-comment these if you want to debug ssl or kerberos)
+#export ALLUXIO_JAVA_OPTS="$ALLUXIO_JAVA_OPTS -Djavax.net.debug=ssl"
+#export ALLUXIO_JAVA_OPTS="$ALLUXIO_JAVA_OPTS -Dsun.security.krb5.debug=true"
+
